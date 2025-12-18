2fbd97765638c340bbcf037f788fb8b2fd211dba
Auzan Fathi Hawari <auzan.fathi@student.umn.ac.id>
2025-12-18 10:53:51 +0700

    fix error part 1

--- server/server.js
+++ server/server.js
@@ -8,29 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
+
+// ROUTES
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/cv", require("./routes/cv"));
 
-// Test endpoint
+// TEST ROOT
 app.get("/", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "CV Builder API is running"
-  });
+  res.send("API RUNNING");
 });
 
-
-// MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error:", err));
-
-// Port (WAJIB untuk cloud)
 const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log("API ready on port " + PORT);
-});
-
+app.listen(PORT, () => console.log("API ready on " + PORT));
