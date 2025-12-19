63461e438909871a3601c14cc17f635723df96a2
Auzan Fathi Hawari <auzan.fathi@student.umn.ac.id>
2025-12-19 11:49:09 +0700

    add jwttoken

--- server/routes/cv.js
+++ server/routes/cv.js
@@ -1,28 +1,47 @@
 const router = require("express").Router();
 const CV = require("../models/CV");
+const auth = require("../middleware/authMiddleware");
 
-// CREATE
-router.post("/", async (req, res) => {
-  const cv = await CV.create(req.body);
-  res.json(cv);
+/**
+ * GET CV milik user yang login
+ */
+router.get("/me", auth, async (req, res) => {
+  try {
+    const cv = await CV.findOne({ userId: req.user.id });
+    res.json(cv);
+  } catch (err) {
+    res.status(500).json({ message: "Gagal load CV" });
+  }
 });
 
-// READ
-router.get("/:userId", async (req, res) => {
-  const cv = await CV.findOne({ userId: req.params.userId });
-  res.json(cv);
+/**
+ * CREATE / UPDATE CV (UPSERT)
+ * Satu user = satu CV
+ */
+router.post("/", auth, async (req, res) => {
+  try {
+    const cv = await CV.findOneAndUpdate(
+      { userId: req.user.id },
+      { ...req.body, userId: req.user.id },
+      { new: true, upsert: true }
+    );
+
+    res.json(cv);
+  } catch (err) {
+    res.status(500).json({ message: "Gagal menyimpan CV" });
+  }
 });
 
-// UPDATE
-router.put("/:id", async (req, res) => {
-  const cv = await CV.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(cv);
-});
-
-// DELETE
-router.delete("/:id", async (req, res) => {
-  await CV.findByIdAndDelete(req.params.id);
-  res.json("CV deleted");
+/**
+ * DELETE CV
+ */
+router.delete("/", auth, async (req, res) => {
+  try {
+    await CV.findOneAndDelete({ userId: req.user.id });
+    res.json({ message: "CV deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Gagal hapus CV" });
+  }
 });
 
 module.exports = router;
